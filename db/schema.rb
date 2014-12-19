9cca3351406194e9f206f43f0114a60478802faf
Davide Puggioni <davide.puggioni@gmail.com>
2014-12-19 19:23:44 +0100

    Added Post Model and Controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217152619) do
+ActiveRecord::Schema.define(version: 20141219182246) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
