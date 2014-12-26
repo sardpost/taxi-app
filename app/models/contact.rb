a4ee5bc58caab6b30f938d96d5242bb153753f94
Davide Puggioni <davide.puggioni@gmail.com>
2014-12-26 23:35:07 +0100

    Added validation to message text area in Contact page. It can't be blank

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < MailForm::Base
   
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
+  attribute :message,	:validate => true
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
